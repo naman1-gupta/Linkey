981c000472b10b22c8f7a897bf137d57c9dbbb46
naman <namangupta111@gmail.com>
2017-10-26 02:37:13 +0530

    votes counter added

--- linkey/models.py
+++ linkey/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models import Count
+
+class LinkVoteCountManager(models.Manager):
+    def get_queryset(self):
+        return super(LinkVoteCountManager, self).get_queryset().annotate(votes=Count('vote')).order_by("-votes")
+
 
 class Link(models.Model):
     title = models.CharField("Headline", max_length=100)
@@ -8,6 +14,9 @@ class Link(models.Model):
     rank_score = models.FloatField(default=0.0)
     url = models.URLField("URL", max_length=250, blank=True)
     description = models.TextField(blank=True)
+
+    with_votes = LinkVoteCountManager()
+    objects = models.Manager() #default manager
 
     def __unicode__(self):
         return self.title
--- linkey/views.py
+++ linkey/views.py
@@ -3,6 +3,7 @@ from django.views.generic import ListView
 from .models import Link, Vote
 
 class LinkListView(ListView):
-    model = Link
     template_name = 'linkey/links.html'
+    model = Link
     context_object_name = 'links'
+    queryset = Link.with_votes.all()
